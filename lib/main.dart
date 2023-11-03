bc3c06592c9bb53aaea9a5bc06ee2925226bd9ac
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-11-03 17:43:22 +0100

    update route

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const LoginScreen(),
-      home: const SelectLevelScreen(),
+      home: const LoginScreen(),
     );
   }
 }
